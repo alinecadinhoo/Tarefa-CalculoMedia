b09003a6788a7a95e4918beace65d0fff34bebb5
Aline Cadinho <cadinhoaline@gmail.com>
2023-05-03 19:31:31 -0300

    calculomedia

---
+++ CalculoMedia.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CalculoMedia {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota:");
+        double nota1 = scanner.nextDouble();
+
+        System.out.println("Digite a segunda nota:");
+        double nota2 = scanner.nextDouble();
+
+        System.out.println("Digite a terceira nota:");
+        double nota3  = scanner.nextDouble();
+
+        System.out.println("Digite a quarta nota:");
+        double nota4 = scanner.nextDouble();
+
+        double media = (nota1 + nota2 + nota3 + nota4) /4;
+
+        System.out.println("A média das notas são: " + media);
+
+        if (media >= 6) {
+            System.out.println("Parabéns!!! Você foi Aprovado(a)!");
+
+        } else
+                System.out.println("Reprovado(a)!");
+        }
+    }
